0c5ea9518620f17aa56c6fbb891a441612f0c6df
unknown <Administrator@.(none)>
2010-10-23 10:56:33 +0530

    total application import to git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterSample::Application.config.secret_token = '19f6773b26c9934ad3899433a026b19d3ceae764f4f707244dcb57859a7c344fa0dde1847a1af28dd935fbd5042c15309d35f02c769fa0cc32cf99ea235615df'
